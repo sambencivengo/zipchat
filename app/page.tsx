af7dc1407bf4e10f9c5ed1ab9281eedcbca1c7c0
Sam Bencivengo <sambencivengo@gmail.com>
2024-09-16 07:56:37 -0600

    linting and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import {FlaskForm} from "~/app/components/flaskForm";
-import {PageLayout} from "~/app/components/layouts/pageLayout";
-import {cn} from "~/lib/utils";
+import { FlaskForm } from '~/app/components/flaskForm';
+import { PageLayout } from '~/app/components/layouts/pageLayout';
+import { cn } from '~/lib/utils';
 
 export default async function Home() {
   return (
